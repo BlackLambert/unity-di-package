57ddfb8aa72c2d1d94f9f5c80cb9b9facb68597d
Sebastian Baier <sebastian.baier@sandbox.game>
2023-12-25 22:03:56 +0100

    Fixes DeepObjectActivator

--- Runtime/Scripts/Core/Pooling/DeepObjectActivator.cs
+++ Runtime/Scripts/Core/Pooling/DeepObjectActivator.cs
@@ -4,42 +4,51 @@ using UnityEngine;
 namespace SBaier.DI
 {
     public class DeepObjectActivator : ObjectActivator
-
     {
-    private Stack<Transform> _stack = new Stack<Transform>();
+        private Stack<Transform> _stack = new Stack<Transform>();
 
-    public void Activate(GameObject gameObject)
-    {
-        EnableInternal(gameObject.transform);
-
-        _stack.Clear();
-    }
-
-    public void Disable(GameObject gameObject)
-    {
-        DisableInternal(gameObject.transform);
-
-        _stack.Clear();
-    }
-
-    private void EnableInternal(Transform transform)
-    {
-        foreach (Transform child in transform)
+        public void Activate(GameObject gameObject)
         {
-            DisableInternal(child);
+            AddToEnableStack(gameObject.transform);
+
+            while (_stack.Count > 0)
+            {
+                _stack.Pop().gameObject.SetActive(true);
+            }
+
+            _stack.Clear();
         }
 
-        _stack.Push(transform);
-    }
-
-    private void DisableInternal(Transform transform)
-    {
-        _stack.Push(transform);
-
-        foreach (Transform child in transform)
+        public void Disable(GameObject gameObject)
         {
-            DisableInternal(child);
+            AddToDisableStack(gameObject.transform);
+
+            while (_stack.Count > 0)
+            {
+                _stack.Pop().gameObject.SetActive(false);
+            }
+
+            _stack.Clear();
         }
-    }
+
+        private void AddToEnableStack(Transform transform)
+        {
+            foreach (Transform child in transform)
+            {
+                AddToEnableStack(child);
+            }
+
+            _stack.Push(transform);
+        }
+
+        private void AddToDisableStack(Transform transform)
+        {
+            _stack.Push(transform);
+
+            foreach (Transform child in transform)
+            {
+                AddToDisableStack(child);
+            }
+        }
     }
 }
