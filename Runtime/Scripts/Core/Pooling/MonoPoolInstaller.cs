88a30de14bacff3d3953996afd48a9c6eb2130b4
Sebastian Baier <sebastian.baier@sandbox.game>
2024-01-15 18:26:50 +0100

    Fixes name of the MonoPoolInstaller prefab

--- Runtime/Scripts/Core/Pooling/MonoPoolInstaller.cs
+++ Runtime/Scripts/Core/Pooling/MonoPoolInstaller.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace SBaier.DI
@@ -7,18 +5,18 @@ namespace SBaier.DI
     public class MonoPoolInstaller<TItem> : MonoInstaller where TItem : Component
     {
         [SerializeField]
-        private TItem _fooPrefab;
+        private TItem _prefab;
 
         public override void InstallBindings(Binder binder)
         {
             binder.Bind<Factory<TItem>>()
                 .And<Factory<TItem, Transform>>()
                 .ToNew<PrefabFactory<TItem>>()
-                .WithArgument(_fooPrefab);
+                .WithArgument(_prefab);
             
             binder.Bind<Pool<TItem>>()
                 .ToNew<MonoPool<TItem>>()
-                .WithArgument(_fooPrefab)
+                .WithArgument(_prefab)
                 .AsSingle();
         }
     }
@@ -26,18 +24,18 @@ namespace SBaier.DI
     public class MonoPoolInstaller<TItem, TArgument> : MonoInstaller where TItem : Component
     {
         [SerializeField]
-        private TItem _fooPrefab;
+        private TItem _prefab;
 
         public override void InstallBindings(Binder binder)
         {
             binder.Bind<Factory<TItem, TArgument>>()
                 .And<Factory<TItem, TArgument, Transform>>()
                 .ToNew<PrefabFactory<TItem, TArgument>>()
-                .WithArgument(_fooPrefab);
+                .WithArgument(_prefab);
             
             binder.Bind<Pool<TItem, TArgument>>()
                 .ToNew<MonoPool<TItem, TArgument>>()
-                .WithArgument(_fooPrefab)
+                .WithArgument(_prefab)
                 .AsSingle();
         }
     }
